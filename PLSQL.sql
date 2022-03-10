ee807e9dff90909b16013fad7a451a8b22547942
Your Name <you@example.com>
2022-03-10 20:00:42 +0100

    start

--- PLSQL.sql
+++ PLSQL.sql
@@ -1,28 +1,28 @@
-USE test;
+DROP DATABASE IF EXISTS exercicis_plsql;
+CREATE DATABASE exercicis_plsql;
+USE exercicis_plsql;
+-- Escriu un procediment que rebi un nombre real d'entrada
+--  i mostri un missatge indicant si el número és positiu,
+--  negatiu o zero.
+DROP PROCEDURE IF EXISTS prueba;
+DELIMITER //
+CREATE PROCEDURE prueba(IN num DECIMAL, OUT resultat VARCHAR(20))
+BEGIN
+    IF num >= 0 AND num < 5 THEN
+        SELECT resultat = "Insuficient";
+    ELSE IF num < 6 THEN
+        SELECT resultat = "Aprovat";
+    ELSE IF num < 7 THEN
+        SELECT resultat = "Bé";
+    ELSE IF num < 9 THEN
+        SELECT resultat = "Notable";
+    ELSE IF num <= 10 THEN
+        SELECT resultat = "Excel·lent";
+    ELSE
+        SELECT resultat = "Nota no valida";
+    END IF;
+END //
 
-DROP PROCEDURE IF EXIST sign;
+/* ----------- PRUEBAS ----------- */
 
-DELIMITER / / 
-CREATE PROCEDURE sign(IN numero INT, OUT respuesta varchar(14))
-BEGIN CASE
-WHEN numero > 0 Then
-SET
-    respuesta = "Es positivo";
-
-ELSE
-WHEN numero < 0 Then
-SET
-    respuesta = "Es negativo";
-
-ELSE
-SET
-    respuesta = "Es cero";
-
-END CASE;
-
-END / / DELIMETER;
-
-CALL sign(7, @respuesta);
-
-SELECT
-    @respuesta;
+SET resultat
