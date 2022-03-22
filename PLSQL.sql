4a448ce422c0058d21194062b86cd80784de639e
Your Name <you@example.com>
2022-03-22 17:26:11 +0100

    test

--- PLSQL.sql
+++ PLSQL.sql
@@ -1,20 +1,29 @@
-DROP DATABASE IF EXISTS exercicis_plsql;
-CREATE DATABASE exercicis_plsql;
-USE exercicis_plsql;
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE test;
+USE test;
  
-DROP FUNCTION IF EXISTS gnums;
-DELIMITER //
-CREATE FUNCTION gnums(num1 INT, num2 INT, num3 INT)
-RETURNS INT DETERMINISTIC
-BEGIN
-    DECLARE num INT;
-    CASE
-       WHEN num1 > num2 AND num1 > num3 THEN SET num = num1;
-       WHEN num2 > num1 AND num2 > num3 THEN SET num = num2;
-       ELSE SET num = num3;
-   END CASE;
-return (num);
-END //
+DROP TABLE IF EXISTS alumnes;
+CREATE TABLE alumnes(
+   id INT PRIMARY KEY AUTO_INCREMENT,
+   nom CHAR(20),
+   cognom1 CHAR(20),
+   cognom2 CHAR(20),
+   nota INT 
+);
 
-/* ----------- PRUEBAS ----------- */
-SELECT gnums(22, 3, 4);3
+DELIMETER $$
+
+CREATE TRIGGER trigger_check_nota_before_insert
+   BEFORE INSERT
+   ON alumnes FOR EACH ROW
+BEGIN   
+   IF new.nota < 0 THEN 
+   SET new.nota = 0;
+   END IF;
+
+   IF new.nota > 10 THEN 
+   SET new.nota = 10;
+   END IF;
+END$$
+
+DELIMETER;
