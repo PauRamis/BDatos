673c787accaa7494c06305731430990b869326da
Your Name <you@example.com>
2022-04-01 16:48:07 +0200

    in procedure

--- Trigger2.sql
+++ Trigger2.sql
@@ -15,7 +15,20 @@ CREATE TABLE alumnes (
 
 DROP PROCEDURE IF EXISTS crear_email;
 DELIMITER //
-CREATE PROCEDURE crear_email(IN gmail CHAR)
+CREATE PROCEDURE crear_email(IN nom CHAR(20), IN cognom1 CHAR(20), IN cognom2 CHAR(20), IN domini CHAR(20), OUT gmail VARCHAR(20))
 BEGIN
- 
+   SELECT SUBSTRING(nom, 1, 1) AS ExtractString;
+   SET gmail = ExtractString;
 END //
+
+/* El primer caràcter de l'paràmetre nom.
+Els tres primers caràcters de l'paràmetre cognom1.
+Els tres primers caràcters de l'paràmetre cognom2.
+El caràcter @.
+El domini passat com a paràmetre. */
+
+
+
+/* inserts */
+INSERT INTO `alumnes` 
+VALUES (1,'et','soluta','Elise',5,'blala'),(2,'in','quae','Jamie',-1,'blala'),(3,'et','porro','Isaiah',629,'blala'); 
