e850ebbfc7c0ef04ee3786e44db5a76a5de5413d
Your Name <you@example.com>
2022-04-05 15:32:09 +0200

    TRIGGER2

--- Trigger.sql
+++ Trigger.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS test;
 CREATE DATABASE test;
-USE test;x
+USE test;
 
 DROP TABLE IF EXISTS alumnes;
 CREATE TABLE alumnes (
@@ -50,43 +50,18 @@ BEGIN
 END$$
 
 /*Trigger2*/
-CREATE TRIGGER trigger_check_nota_before_insert
-   BEFORE INSERT ON alumnes
-   FOR EACH ROW
-BEGIN
-   IF new.nota < 0 THEN
-   SET new.nota = 0;
-   END IF;
-
-   IF new.nota > 10 THEN
-   SET new.nota = 10;
-   END IF; 
-END$$
-
-CREATE TRIGGER trigger_check_nota_before_update
-   BEFORE UPDATE ON alumnes
-   FOR EACH ROW
-BEGIN
-   IF new.nota < 0 THEN
-   SET new.nota = 0;
-   END IF;
-
-   IF new.nota > 10 THEN
-   SET new.nota = 10;
-   END IF;
-END$$
 
 CREATE TRIGGER trigger_crear_email_before_insert
    BEFORE INSERT ON alumnes
    FOR EACH ROW
 BEGIN
-   IF new.gmail = NULL THEN
-      CALL crear_email(nom, cognom1, cognom2, domini, new.gmail);
+   IF new.gmail IS NULL THEN
+      CALL crear_email(new.nom, new.cognom1, new.cognom2, "domini", new.gmail);
    END IF;
 END$$
 
 /*Trigger3*/
-
+/*Trigger4*/
 /* inserts */
 INSERT INTO `alumnes` (id, nom, cognom1, cognom2, nota, domini)
 VALUES (1,'et','soluta','Elise',5,'blala'),(2,'in','quae','Jamie',-1,'blala'),(3,'et','porro','Isaiah',629,'blala'); 
