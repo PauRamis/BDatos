926dd27a1323133895e33e85890988a9e46b6c1c
Your Name <you@example.com>
2022-05-10 15:58:47 +0200

    jdbc

---
+++ consultes.sql
@@ -0,0 +1,7 @@
+SELECT nomClient, correu, numClient FROM Client WHEN ;
+SELECT descripció, PVP, IVA FROM Producte WHEN ;
+SELECT nombreUnitats FROM Producte_Compra WHEN;
+SELECT percentatge FROM Historial_Descomptes;
+SELECT númeroTargeta FROM Targeta WHEN ;
+SELECT carrer, num, codiPostal, codiCiutat, codiAdreça FROM Adreça WHEN ;
+SELECT nom FROM Ciutat WHEN ;
