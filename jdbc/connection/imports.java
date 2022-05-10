84bc16040c666c1abcdbaf4362d6d43f4a741a91
Your Name <you@example.com>
2022-05-10 16:36:16 +0200

    idk

---
+++ jdbc/connection/imports.java
@@ -0,0 +1,7 @@
+package connection;
+
+public class imports {
+    url = "";
+    username = "";
+    password = "";
+}
---
+++ jdbc/dao/AutorDao.java
@@ -0,0 +1,7 @@
+package dao;
+
+import domain.Autor;
+
+public interface AutorDao {
+    Autor findById(int id);
+}
